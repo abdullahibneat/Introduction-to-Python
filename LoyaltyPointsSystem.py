35fe75f43b261a9217edc16fbbdaae910605c1c7
Abdullah Ibne Atiq <abdullahibneat@users.noreply.github.com>
2018-11-09 10:02:02 +0000

    Added a second while loop
    Previously, the customer would be automatically logged out after the user buys coffee but doesn't yet hold 10 points. The second while loop will allow the customer to decide when to log out.

--- LoyaltyPointsSystem.py
+++ LoyaltyPointsSystem.py
@@ -4,18 +4,18 @@ from random import randint
 
 # Intro
 print("""\
-++++++++++++++++++++++++++++
-|      WELCOME TO THE      |
-|   UNIVERSITY COFFE SHOP  |
-++++++++++++++++++++++++++++
++---------------------------+
+|       WELCOME TO THE      |
+|   UNIVERSITY COFFEE SHOP  |
++---------------------------+
 """)
 sleep(1)
 
 # Improvement notification
 print("""\
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++--------------------------------------------------------+
 | To improve our shop, we now have a loyalty card system |
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++--------------------------------------------------------+
 """)
 sleep(0.75)
 
@@ -34,14 +34,13 @@ sleep(0.5)
 # Explanation of the point sysem
 print("""\
 
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-| Each time you purchase a coffe, you get * 2 * points |
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++-------------------------------------------------------+
+| Each time you purchase a coffee, you get * 2 * points |
++-------------------------------------------------------+
 """)
 sleep(0.5)
-print("""\
-|  You get a free coffe when you reach * 10 * points.  |
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+print("""|  You get a free coffee when you reach * 10 * points.  |
++-------------------------------------------------------+
 """)
 sleep(1)
 
@@ -56,13 +55,13 @@ while (totalpoints >= 10):
         freecoffeetext = 'free coffee! |'
 
     # Show a message
-    print('====================================================')
+    print('================================================')
     print('|  Congratulations! You are due', freecoffee, freecoffeetext)
-    print('====================================================')
+    print('================================================')
     sleep(0.5)
 
     # Ask if customer wants the free coffee(s)
-    askcoffee = input('Would you like 1 free coffe now (yes / no)? ')
+    askcoffee = input('Would you like 1 free coffee now (yes / no)? ')
 
     # If customer wants free coffee
     if (askcoffee == 'yes'):
@@ -94,66 +93,73 @@ while (totalpoints >= 10):
 
     # Otherwise
     else:
-        print('See you soon!')
         break
 
 # If point total to less than 10
 else:
+    while True:
+        # Prompt to buy coffee
+        askcoffee = input('Would you like to buy 1 coffee now? (yes / no) ')
 
-    # Prompt to buy coffe
-    askcoffee = input('Would you like to buy 1 coffe now? (yes / no) ')
+        # If customer wants coffee
+        if (askcoffee == 'yes'):
 
-    # If customer wants coffee
-    if (askcoffee == 'yes'):
-
-        # Give coffee
-        print('Here you go')
-        sleep(0.4)
-        print("""
-            ..
-          ..  ..
-                ..
-                 ..
-                ..
-               ..
-             ..
-    ##       ..    ####
-    ##.............##  ##
-    ##.............##   ##
-    ##.............## ##
-    ##.............###
-     ##...........##
-      #############
-      #############
-    #################
-    """)
-
-        # Award 2 points
-        totalpoints = totalpoints + 2
-        print('You now have', totalpoints, 'points.')
-
-        # If point are now >= 10
-        if (totalpoints >= 10):
-            onecoffee = input('You are due 1 free coffee! Would you like it now? (yes / no): ')
-            if (onecoffee == 'yes'):
-                totalpoints = totalpoints - 10
-                print('Here you go')
-                sleep(0.4)
-                print("""
-                ..
-              ..  ..
-                    ..
-                     ..
-                    ..
+            # Give coffee
+            print('Here you go')
+            sleep(0.4)
+            print("""
+              ..
+            ..  ..
+                  ..
                    ..
+                  ..
                  ..
-        ##       ..    ####
-        ##.............##  ##
-        ##.............##   ##
-        ##.............## ##
-        ##.............###
-         ##...........##
-          #############
-          #############
-        #################
-        """)
+               ..
+      ##       ..    ####
+      ##.............##  ##
+      ##.............##   ##
+      ##.............## ##
+      ##.............###
+       ##...........##
+        #############
+        #############
+      #################
+      """)
+            sleep(0.2)
+
+            # Award 2 points
+            totalpoints = totalpoints + 2
+            print('You now have', totalpoints, 'points.')
+
+            # If point are now >= 10
+            if (totalpoints >= 10):
+                onecoffee = input('You are due 1 free coffee! Would you like it now? (yes / no): ')
+                if (onecoffee == 'yes'):
+                    totalpoints = totalpoints - 10
+                    print('Here you go')
+                    sleep(0.4)
+                    print("""
+                  ..
+                ..  ..
+                      ..
+                       ..
+                      ..
+                     ..
+                   ..
+          ##       ..    ####
+          ##.............##  ##
+          ##.............##   ##
+          ##.............## ##
+          ##.............###
+           ##...........##
+            #############
+            #############
+          #################
+          """)
+                    sleep(0.2)
+                else:
+                    print('Have a nice day!')
+                    break
+        if (askcoffee == 'no'):
+            print('Have a nice day!')
+            break
