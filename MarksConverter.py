a9226c33440bd62e51e1f2efc0aada6b5433d1b8
Abdullah Ibne Atiq <abdullahibneat@users.noreply.github.com>
2018-11-10 21:34:47 +0000

    Run program under while loop
    The code has been put under a while loop so that it's easier to grade multiple exams consecutively. Entering marks "0" and "0" will terminate the program.

--- MarksConverter.py
+++ MarksConverter.py
@@ -1,46 +1,66 @@
-# Input exam marks
-Exam = int(input('Exam marks: '))
+# Program name
+print("""
++----------------------+
+| Exam marks converter |
++----------------------+
+""")
+while True:
+    # Input exam marks
+    Exam = int(input('Exam marks: '))
 
-# Input assignment marks
-Assignment = int(input('Assignment marks: '))
+    # Input assignment marks
+    Assignment = int(input('Assignment marks: '))
 
-# If exam mark > 100
-while (Exam > 100):
-    # Keep asking for correct mark.
-    Exam = int(input('Error: Exam marks can\'t exceed 100 marks. Please enter correct marks: '))
+    # If exam mark > 100
+    while (Exam > 100):
+        # Keep asking for correct mark.
+        Exam = int(input('Error: Exam marks can\'t exceed 100 marks. Please enter correct marks: '))
 
-# If assignment mark > 100
-while (Assignment > 100):
-    # Keep asking for correct mark
-    Assignment = int(input('Error: Assignment marks can\'t exceed 100 marks. Please enter correct marks: '))
+    # If assignment mark > 100
+    while (Assignment > 100):
+        # Keep asking for correct mark
+        Assignment = int(input('Error: Assignment marks can\'t exceed 100 marks. Please enter correct marks: '))
 
-# Multiply marks by weight percentage
-ExamMarks = Exam * 0.6
-AssignmentMarks = Assignment * 0.4
+    # Multiply marks by weight percentage
+    ExamMarks = Exam * 0.6
+    AssignmentMarks = Assignment * 0.4
 
-# Calculate overall marks
-Grade = int(ExamMarks + AssignmentMarks)
+    # Calculate overall marks
+    Grade = int(ExamMarks + AssignmentMarks)
 
-# Marks >= 90
-if (Grade >= 80):
+    # Marks >= 90
+    if (Grade >= 80):
 
-    # Grade A
-    print(Grade, 'marks. A grade')
+        # Grade A
+        print(Grade, 'marks. A grade')
 
-# Marks between 60 and 80
-elif (60 <= Grade < 80):
+    # Marks between 60 and 80
+    elif (60 <= Grade < 80):
 
-    # Grade B
-    print(Grade, 'marks. B grade')
+        # Grade B
+        print(Grade, 'marks. B grade')
 
-# Marks between 40 and 60
-elif (40 <= Grade < 60):
+    # Marks between 40 and 60
+    elif (40 <= Grade < 60):
 
-    # Grade C
-    print(Grade, 'marks. C grade')
+        # Grade C
+        print(Grade, 'marks. C grade')
 
-# Grade less than 40
-else:
+    # Grade less than 40
+    else:
 
-    # Retake exam
-    print(Grade, 'marks. Retake')
+        # Retake exam
+        print(Grade, 'marks. Retake')
+
+    # Add a space between the while instances
+    if (Grade):
+
+        # Print a blank space
+        print(' ')
+
+        # Let the while loop continue after the blank space
+        continue
+
+    # Otherwise terminate execution
+    else:
+        break
